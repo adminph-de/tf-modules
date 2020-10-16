8dd16bb59b0e712bafe424a2e4b1ea7b0ba0e882
Patrick Hayo <36226934+adminph-de@users.noreply.github.com>
2020-10-16 16:32:09 +0200

    Update variables.tf

--- azure/mssql_paas/variables.tf
+++ azure/mssql_paas/variables.tf
@@ -1,31 +1,29 @@
-variable "name" {
+variable "server_name" {
   type        = string
   description = "Name of the SQL Server"
 }
-
 variable "resource_group_name" {
   type        = string
   description = "Resourcegroup Name where the SQL server gets deployed"
 }
-
-variable "region" {
+variable "location" {
   type        = string
   description = "Azuer region"
 }
-
 variable "sku" {
   type        = string
   description = "SQL Server version"
   default     = "12.0"
 }
-
 variable "admin_login" {
   type        = string
   description = "SQL Server Administratior Username"
-  default     = "sqladmin"
 }
-
 variable "admin_password" {
   type        = string
   description = "SQL Server Administrator Password"
+}
+variable "db_name" {
+  type        = string
+  description = "Name of the SQL Server"
 }
