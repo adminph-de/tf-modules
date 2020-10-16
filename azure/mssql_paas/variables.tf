f331717e4dc8332dc2e95be3290503bcb521e385
adminph-de <36226934+adminph-de@users.noreply.github.com>
2020-10-17 01:03:39 +0200

    changes

--- azure/mssql_paas/variables.tf
+++ azure/mssql_paas/variables.tf
@@ -23,7 +23,38 @@ variable "admin_password" {
   type        = string
   description = "SQL Server Administrator Password"
 }
+## Database Name
 variable "db_name" {
   type        = string
   description = "Name of the SQL Server"
+}
+## Storage Account
+variable "primary_blob_endpoint" {
+  type        = string
+  description = "Storage Account BLOB Endpoint URL"
+}
+variable "primary_access_key" {
+  type        = string
+  description = "Storage Account primary KEY"
+}
+## Tags
+variable "tag_owner" {
+  type        = string
+  description = "ApplicationOwner"
+  default     = null
+}
+variable "tag_project" {
+  type        = string
+  description = "Project Name or Description"
+  default     = "Terraform Deployment"
+}
+variable "tag_environment" {
+  type        = string
+  description = "Environment: production, testing, development, sandbox"
+  default     = "production"
+}
+variable "tag_description" {
+  type        = string
+  description = "Storage Account Shared Folders"
+  default     = "Created by Terraform"
 }
