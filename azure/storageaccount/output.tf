c9d06e4b906514e4e93e4614ffc1a920b262979b
adminph-de <36226934+adminph-de@users.noreply.github.com>
2020-10-16 18:14:54 +0000

    changed

--- azure/storageaccount/output.tf
+++ azure/storageaccount/output.tf
@@ -1,3 +1,6 @@
-output "terraform_state_storage_account" {
-  value = azurerm_storage_account.sta.name
+output "primary_blob_endpoint" {
+  value = "${azurerm_storage_account.sta.primary_blob_endpoint}"
+}
+output "primary_access_key" {
+  value = "${azurerm_storage_account.sta.primary_access_key}"
 }
