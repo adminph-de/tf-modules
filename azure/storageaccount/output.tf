75dd7d0be902c668370653c886a027da564fd540
adminph-de <36226934+adminph-de@users.noreply.github.com>
2020-10-16 18:33:11 +0000

    changed

--- azure/storageaccount/output.tf
+++ azure/storageaccount/output.tf
@@ -1,6 +1,6 @@
 output "primary_blob_endpoint" {
-  value = "${azurerm_storage_account.sta.primary_blob_endpoint}"
+  value = "${ azurerm_storage_account.sta.primary_blob_endpoint }"
 }
 output "primary_access_key" {
-  value = "${azurerm_storage_account.sta.primary_access_key}"
+  value = "${ azurerm_storage_account.sta.primary_access_key }"
 }
